3c8a9c6da5cc99c719b69372200972ec3b291920
Matthew Cibulka <mcibulkadev@gmail.com>
2018-09-15 10:56:53 -0400

    Inserts scraped data into db.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cheerio = require("cheerio");
 const request = require("request");
 const scrapeURL = "https://www.thestar.com";
 
-const databaseURL = "scraper";
+const databaseURL = "scraperDB";
 const collections = ["scrapedData"];
 
 const db = mongojs(databaseURL, collections);
@@ -25,28 +25,32 @@ app.get("/scrape", (req, res) => {
   request(scrapeURL, (error, response, html) => {
     const $ = cheerio.load(html);
 
-    let results = [];
-
-    $("div", ".small-story--small").each((i, element) => {
-      const articleDiv = $(element).html();
-      const headline = $(articleDiv).find(".story__headline").text();
-      const abstract = $(articleDiv).find(".story__abstract").text();
-      const link = scrapeURL + $(articleDiv).find("a").attr("href");
+    $("div .story__body").each((i, element) => {
+      // console.log(element);
+      const headline = $(element).find(".story__headline").text();
+      const abstract = $(element).find(".story__abstract").text();
+      const link = scrapeURL + $(element).find("a").attr("href");
 
       // skip any items that are missing a headline or abstract
       if (headline && abstract) {
-        results.push({
+        db.scrapedData.insert({
           headline: headline,
           abstract: abstract,
           link: link
-        });
+        },
+          (error, inserted) => {
+            if (error) {
+              console.log(error);
+            }
+            else {
+              console.log(inserted);
+            }
+          });
       }
     });
-
-    results.forEach(result => {
-      console.log(`${result.headline.toUpperCase()}\n${result.abstract}\n${result.link}\n\n`);
-    });
   });
+
+  res.end();
 });
 
 
